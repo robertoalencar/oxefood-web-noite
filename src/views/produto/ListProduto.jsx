432c2cc7b749a38cdc133184c4d8d5f10e477b36
Roberto Alencar <rroberto.alencar@gmail.com>
2023-06-05 19:50:36 -0300

    Enviando arquivos do C17 e C18

--- src/views/produto/ListProduto.jsx
+++ src/views/produto/ListProduto.jsx
@@ -1,14 +1,16 @@
 import axios from 'axios';
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Divider, Icon, Table } from 'semantic-ui-react';
+import { Button, Container, Divider, Header, Icon, Modal, Table } from 'semantic-ui-react';
 import { ENDERECO_SERVIDOR } from '../../util/Constantes';
 
 class ListProduto extends React.Component{
 
     state = {
 
-        listaProdutos: []
+        listaProdutos: [],
+        openModal: false,
+        idRemover: null,
     }
 
     componentDidMount = () => {
@@ -27,6 +29,44 @@ class ListProduto extends React.Component{
             })
         })
 
+    };
+
+    confirmaRemover = (id) => {
+
+        this.setState({
+            openModal: true,
+            idRemover: id
+        })  
+    }
+
+    setOpenModal = (val) => {
+
+        this.setState({
+            openModal: val
+        })
+   
+    };
+
+    remover = async () => {
+
+        await axios.delete(ENDERECO_SERVIDOR + '/api/produto/' + this.state.idRemover)
+        .then((response) => {
+   
+            this.setState({ openModal: false })
+            console.log('Produto removido com sucesso.')
+   
+            axios.get(ENDERECO_SERVIDOR + "/api/produto")
+            .then((response) => {
+           
+                this.setState({
+                    listaProdutos: response.data
+                })
+            })
+        })
+        .catch((error) => {
+            this.setState({  openModal: false })
+            console.log('Erro ao remover um produto.')
+        })
     };
 
     render(){
@@ -98,7 +138,8 @@ class ListProduto extends React.Component{
                                                    circular
                                                    icon='trash'
                                                    color='red'
-                                                   title='Clique aqui para remover este cliente' />
+                                                   title='Clique aqui para remover este cliente' 
+                                                   onClick={e => this.confirmaRemover(p.id)} />
 
                                             </Table.Cell>
                                        </Table.Row>
@@ -109,6 +150,27 @@ class ListProduto extends React.Component{
                        </div>
                    </Container>
                </div>
+
+               <Modal
+                    basic
+                    onClose={() => this.setOpenModal(false)}
+                    onOpen={() => this.setOpenModal(true)}
+                    open={this.state.openModal}
+                >
+                    <Header icon>
+                            <Icon name='trash' />
+                            <div style={{marginTop: '5%'}}> Tem certeza que deseja remover esse registro? </div>
+                    </Header>
+                    <Modal.Actions>
+                            <Button basic color='red' inverted onClick={() => this.setOpenModal(false)}>
+                                <Icon name='remove' /> Não
+                            </Button>
+                            <Button color='green' inverted onClick={() => this.remover()}>
+                                <Icon name='checkmark' /> Sim
+                            </Button>
+                    </Modal.Actions>
+                </Modal>
+                
            </div>
        )
    }
