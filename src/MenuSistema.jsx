067135a7552d25d8560b57403d7f628e98a2e546
Roberto Alencar <rroberto.alencar@gmail.com>
2024-08-17 16:17:41 -0300

    enviando arquivos

--- src/MenuSistema.jsx
+++ src/MenuSistema.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
+import { logout } from './views/util/AuthenticationService';
 
 export default function MenuSistema (props) {
 
@@ -42,6 +43,15 @@ export default function MenuSistema (props) {
                    as={Link}
                    to='/list-cupomdesconto'
                 />
+
+                <Menu.Item
+                    className='navbar__item--mobile'
+                    onClick={logout}
+                    content='Sair'
+                    as={Link}
+                    to='/'
+                />
+
 
            </Menu>
        </>
--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -7,21 +7,57 @@ import FormCupomDesconto from './views/cupom/FormCupomDesconto';
 import ListCupomDesconto from './views/cupom/ListCupomDesconto';
 import FormEntregador from './views/entregador/FormEntregador';
 import Home from './views/home/Home';
+import FormLogin from './views/login/FormLogin';
 import FormProduto from './views/produto/FormProduto';
 import ListProduto from './views/produto/ListProduto';
+import { ProtectedRoute } from './views/util/ProtectedRoute';
 
 function Rotas() {
     return (
         <>
             <Routes>
-                <Route path="/" element={ <Home/> } />
-                <Route path="form-cliente" element={ <FormCliente/> } />
-                <Route path="list-cliente" element={ <ListCliente/> } />
-                <Route path="form-produto" element={ <FormProduto/> } />
-                <Route path="list-produto" element={ <ListProduto/> } />
-                <Route path="form-entregador" element={ <FormEntregador/> } />
-                <Route path="form-cupomdesconto" element={ <FormCupomDesconto/> } />
-                <Route path="list-cupomdesconto" element={ <ListCupomDesconto/> } />
+                <Route path="/" element={ <FormLogin/> } />
+
+                <Route path="/home" element={ 
+                        <ProtectedRoute> <Home/> </ProtectedRoute> 
+                    } 
+                />
+                
+                <Route path="form-cliente" element={ 
+                        <ProtectedRoute> <FormCliente/> </ProtectedRoute>
+                    } 
+                />
+
+                <Route  path="/list-cliente" element={
+                        <ProtectedRoute> <ListCliente /> </ProtectedRoute>
+                    }
+                />
+
+                <Route path="form-produto" element={ 
+                        <ProtectedRoute> <FormProduto/> </ProtectedRoute>
+                    } 
+                />
+                
+                <Route path="list-produto" element={ 
+                        <ProtectedRoute> <ListProduto/> </ProtectedRoute> 
+                    } 
+                />
+
+                <Route path="form-entregador" element={ 
+                        <ProtectedRoute> <FormEntregador/> </ProtectedRoute> 
+                    } 
+                />
+
+                <Route path="form-cupomdesconto" element={ 
+                        <ProtectedRoute> <FormCupomDesconto/> </ProtectedRoute>
+                    } 
+                />
+
+                <Route path="list-cupomdesconto" element={ 
+                        <ProtectedRoute> <ListCupomDesconto/> </ProtectedRoute>
+                    } 
+                />
+
             </Routes>
         </>
     )
