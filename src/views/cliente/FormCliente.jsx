6a11c5042fbe74a775697a641f9d2fbd1ec5d014
Roberto Alencar <rroberto.alencar@gmail.com>
2024-11-23 10:14:00 -0300

    enviado

--- src/views/cliente/FormCliente.jsx
+++ src/views/cliente/FormCliente.jsx
@@ -1,16 +1,36 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import InputMask from 'react-input-mask';
+import { Link, useLocation } from "react-router-dom";
 import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 
 export default function FormCliente () {
 
+    const { state } = useLocation();
+    const [idCliente, setIdCliente] = useState();
     const [nome, setNome] = useState();
     const [cpf, setCpf] = useState();
     const [dataNascimento, setDataNascimento] = useState();
     const [foneCelular, setFoneCelular] = useState();
     const [foneFixo, setFoneFixo] = useState();
+
+    useEffect(() => {
+
+        if (state != null && state.id != null) {
+
+            axios.get("http://localhost:8080/api/cliente/" + state.id)
+            .then((response) => {
+                setIdCliente(response.data.id)
+                setNome(response.data.nome)
+                setCpf(response.data.cpf)
+                setDataNascimento(formatarData(response.data.dataNascimento))
+                setFoneCelular(response.data.foneCelular)
+                setFoneFixo(response.data.foneFixo)
+            })
+        }
+
+    }, [state])
 
     function salvar() {
 
@@ -21,16 +41,38 @@ export default function FormCliente () {
 		    foneCelular: foneCelular,
 		    foneFixo: foneFixo
 		}
+
+        if (idCliente != null) { //Alteração:
+
+            axios.put("http://localhost:8080/api/cliente/" + idCliente, clienteRequest)
+            .then((response) => { 
+                console.log('Cliente alterado com sucesso.') 
+            })
+            .catch((error) => { 
+                console.log('Erro ao alter um cliente.') 
+            })
+
+        } else { //Cadastro:
 	
-		axios.post("http://localhost:8080/api/cliente", clienteRequest)
-		.then((response) => {
-		    console.log('Cliente cadastrado com sucesso.')
-		})
-		.catch((error) => {
-		    console.log('Erro ao incluir o um cliente.')
-		})
-        
+            axios.post("http://localhost:8080/api/cliente", clienteRequest)
+            .then((response) => {
+                console.log('Cliente cadastrado com sucesso.')
+            })
+            .catch((error) => {
+                console.log('Erro ao incluir o um cliente.')
+            })
+        }
 	}
+
+    function formatarData(dataParam) {
+
+        if (dataParam === null || dataParam === '' || dataParam === undefined) {
+            return ''
+        }
+
+        let arrayData = dataParam.split('-');
+        return arrayData[2] + '/' + arrayData[1] + '/' + arrayData[0];
+    }
 
     return (
 
@@ -42,7 +84,12 @@ export default function FormCliente () {
 
                 <Container textAlign='justified' >
 
-                    <h2> <span style={{color: 'darkgray'}}> Cliente &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+                    { idCliente === undefined &&
+                        <h2> <span style={{color: 'darkgray'}}> Cliente &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro</h2>
+                    }
+                    { idCliente != undefined &&
+                        <h2> <span style={{color: 'darkgray'}}> Cliente &nbsp;<Icon name='angle double right' size="small" /> </span> Alteração</h2>
+                    }
 
                     <Divider />
 
@@ -118,18 +165,18 @@ export default function FormCliente () {
                         </Form>
                         
                         <div style={{marginTop: '4%'}}>
-
-                            <Button
-                                type="button"
-                                inverted
-                                circular
-                                icon
-                                labelPosition='left'
-                                color='orange'
-                            >
+                            <Link to={'/list-cliente'}>
+                                <Button
+                                    inverted
+                                    circular
+                                    icon
+                                    labelPosition='left'
+                                    color='orange'
+                                >
                                 <Icon name='reply' />
                                 Voltar
-                            </Button>
+                                </Button>
+                            </Link>
                                 
                             <Button
                                 inverted
