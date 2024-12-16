1f2b3ac1881f4f8673802407dfc1092a4b70b604
Roberto Alencar <rroberto.alencar@gmail.com>
2024-12-16 18:53:07 -0300

    Enviando arquivos da aula

--- src/views/cliente/FormCliente.jsx
+++ src/views/cliente/FormCliente.jsx
@@ -4,6 +4,7 @@ import InputMask from 'react-input-mask';
 import { Link, useLocation } from "react-router-dom";
 import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
+import { notifyError, notifySuccess } from '../../views/util/Util';
 
 export default function FormCliente () {
 
@@ -56,10 +57,16 @@ export default function FormCliente () {
 	
             axios.post("http://localhost:8080/api/cliente", clienteRequest)
             .then((response) => {
-                console.log('Cliente cadastrado com sucesso.')
+                notifySuccess('Cliente cadastrado com sucesso.')
             })
             .catch((error) => {
-                console.log('Erro ao incluir o um cliente.')
+                if (error.response.data.errors != undefined) {
+                    for (let i = 0; i < error.response.data.errors.length; i++) {
+                        notifyError(error.response.data.errors[i].defaultMessage)
+                    }
+                } else {
+                    notifyError(error.response.data.message)
+                }
             })
         }
 	}
